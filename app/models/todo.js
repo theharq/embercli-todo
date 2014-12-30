86205d254fec17ad440c92bbe709d0afd08a1fb4
Hildebrando Rueda <harq.soft@gmail.com>
2014-12-30 11:43:14 -0500

    Use fixtures for the todo model
    Define the application adapter as a FixtureAdapter

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,6 +1,18 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
+
+Todo.reopenClass({
+  FIXTURES: [
+    {id: 1, title: 'Start learning ember', isCompleted: true},
+    {id: 2, title: 'Use the latest version of ember-cli', isCompleted: true},
+    {id: 3, title: 'Make a todo mvc app version with ember-cli', isCompleted: false},
+    {id: 4, title: 'Upload to github example', isCompleted: false}
+  ]
+});
+
+
+export default Todo;
