005320025d991fe59b4291d505dfb93ebfe7f5e9
Hildebrando Rueda <harq.soft@gmail.com>
2014-12-30 12:09:43 -0500

    Create a new todo instance
    Use the handlebars helper to set a new value, then in the controller
    get that value and create a new record

---
+++ app/controllers/todos.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+  actions: {
+    createTodo: function(){
+      var title = this.get("newTitle");
+      if(!title){return false;}
+
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      this.set("newTitle", "");
+      todo.save();
+    }
+  }
+});
